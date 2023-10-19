12c45690aada627b19881cf4839bfebb52095bff
Edilberto Morais <edilbertocmorais@gmail.com>
2023-10-18 21:55:36 -0300

    BugFix CORS

--- src/main/java/me/dio/service/UserService.java
+++ src/main/java/me/dio/service/UserService.java
@@ -2,15 +2,6 @@ package me.dio.service;
 
 import me.dio.domain.model.User;
 
-/**
- * Usamos o encapsulamento de código para não expor a implementação do código.
- *
- * @author Edilberto Morais
- */
+public interface UserService extends CrudService<Long, User> {
 
-public interface UserService {
-
-    User findById(Long id);
-
-    User create(User userToCreate);
 }
